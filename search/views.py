28b9b5b983dd3541cf2c81b1ffd5dd3af99c8560
Prithwish <prithwish.nag@gmail.com>
2023-07-26 09:19:31 +0530

    dashboard finished

--- search/views.py
+++ search/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+import json
 
 @login_required(login_url="/")
-def dashboard(request): 
+def dashboard(request):
+    if request.method == "POST":
+        pickupLocation = request.POST['pickup-location']
+        dropoffLocation = request.POST['dropoff-location']
+        if pickupLocation == "" or dropoffLocation == "":
+            messages.error(request, "Enter both pickup and dropoff locations")
+        else:
+            data = json.load(open('search/json/rideOptions.json'))
+            data_string = json.dumps(data)
+            return render(request, 'dashboard.html', {'rideOptions': data_string})
     return render(request, 'dashboard.html')
